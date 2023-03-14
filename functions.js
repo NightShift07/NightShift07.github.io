19979728b579a337245d9db991a03d4674d20bad
NightShift07 <126639817+NightShift07@users.noreply.github.com>
2023-03-14 01:49:48 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,22 +4,13 @@ var opTit = document.getElementsByClassName("contTitDat");
 var i;
 
 for (i = 0; i < opTit.length; i++) {
-    opTit[i].addEventListener("click", );
-}
-
-function desplegar(){
-    this.classList.toggle("desplegado");
-    visible();
-    
-}
-
-
-function() {
-
-    var actual = this.nextElementSibling;
-    if (actual.style.display === "block") {
-        actual.style.display = "none";
-    } else {
-        actual.style.display = "block";
-    }
+    opTit[i].addEventListener("click", function() {
+        this.classList.toggle("desplegado");
+        var actual = this.nextElementSibling;
+        if (actual.style.display === "block") {
+            actual.style.display = "none";
+        } else {
+            actual.style.display = "block";
+        }
+    });
 }
